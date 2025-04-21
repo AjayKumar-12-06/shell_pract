a62587c5a948d3bfa5a0bb56877e380a90a54f32
awsajju <aws.devops.123aws@gmail.com>
2025-04-21 17:24:25 +0530

    using for loop installing mysql package using colors

---
+++ for.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+check_root(){
+    if [ $USERID -ne 0 ]; then
+        echo -e " $R u must have the sudo access to execute this $N"
+        exit 1
+    fi
+}
+
+check_root
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e " $2 installing $R failure $N"
+        exit 1
+    else
+        echo -e "$2 installing $G success $N"
+    fi
+}
+
+for package in $@
+do 
+    dnf list installed $package
+    if [ $? -ne 0 ]; then
+        dnf install $package -y
+        validate $? "installing $package"
+    else
+        echo -e "Already $package is installed $Y Skkipping $N"
+done
