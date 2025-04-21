4f9654fce5fec788afcbf68d56fa93285db73f1a
awsajju <aws.devops.123aws@gmail.com>
2025-04-21 17:18:56 +0530

    using function installing mysql package using colors

---
+++ colors.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+check_root(){
+    if [ $USERID -ne 0 ]; then
+        echo -e " $R u must have the sudo access to execute this $N"
+        exit 1
+    fi
+}
+
+check_root
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo " $2 installing $R failure $N"
+        exit 1
+    else
+        echo "$2 installing $G success $N"
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    echo "mysql is not installed, going to installing mysql"
+        dnf install mysql -y
+        validate $? "installing mysql"
+else
+    echo "mysql already installed $Y Skkipping $N"
+fi
