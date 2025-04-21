f365a68a354cf0ca95fd44c210aec931bdc883bf
awsajju <aws.devops.123aws@gmail.com>
2025-04-21 17:00:38 +0530

    using function installing mysql package

---
+++ funtion.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+check_root (){
+    if [ $USERID -ne 0 ]; then
+        echo "you have must have sudo access to excute this"
+        exit 1
+    fi
+}
+
+check_root
+
+vaidate (){
+    if [ $1 -ne 0 ]; then
+        echo "installing failure"
+        exit 1
+    else
+        echo "isbntalling success"
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+
+echo "mysql is not installed, going to installing mysql"
+    dnf install mysql -y
+        if [ $? -ne 0 ]; then
+            validate $? "installing mysql"
+else
+    echo "mysql already installed"
+fi
