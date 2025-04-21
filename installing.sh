80c4295408fb20d32b071715728f36ba4cafe2b9
awsajju <aws.devops.123aws@gmail.com>
2025-04-21 16:31:31 +0530

    using condition installing mysql package

--- installing.sh
+++ installing.sh
@@ -2,15 +2,15 @@
 
 USERID=$(id -u)
 
-if[ $USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
     echo "You must have the sudo access to execute this"
-    exit
+    exit 1
 fi
 
 dnf list installed mysql
 echo "mysql is not available, now it's going to inbstall mysql "
 
-    if[ $? -ne 0 ]; then
+    if [ $? -ne 0 ]; then
         dnf install mysql -y
             if [ $? -ne 0 ]; then
                 echo "Mysql installing failure"
